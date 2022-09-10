948ead3fb2b8227c44971cee310ece9672763c3e
Lucas <55973521+lucasrouchy@users.noreply.github.com>
2022-09-10 13:26:26 -0700

    starter variables and if statement to see if k covrs over half of the possible buy and sell prices

---
+++ buyandsell.py
@@ -0,0 +1,16 @@
+
+import math
+class Solution:
+    def maxProfit(k: int, prices: 'list[int]') -> int:
+        temp = 0
+        if not prices: return 0
+        profit = 0
+        n = len(prices)
+        #if k is greater than or eqaul to half the len of the prices list
+        if k >= n//2:
+            
+        
+        
+        
+    maxProfit(2 ,[2,4,1])
+    maxProfit(2,[3,2,6,5,0,3])
